304bc8addd639bc4b32f5d69c824c070476eac5b
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2023-01-26 17:29:22 +0000

    finished chapter 6 exercises

--- rango/models.py
+++ rango/models.py
@@ -26,5 +26,8 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
+    class Meta:
+        verbose_name_plural = 'Pages'
+
     def __str__(self):
         return self.title
